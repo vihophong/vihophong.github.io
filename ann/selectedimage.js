b3670a43d98363adffaffd727b78b3a3aae67db0
vihophong <hophongmc@gmail.com>
2020-04-04 01:30:56 +0900

    aa

---
+++ ann/selectedimage.js
@@ -0,0 +1,11 @@
+$("#image-selector").change(function () {
+	let reader = new FileReader();
+	reader.onload = function () {
+		let dataURL = reader.result;
+		$("#selected-image").attr("src", dataURL);
+		$("#prediction-list").empty();
+	}
+	
+	let file = $("#image-selector").prop('files')[0];
+	reader.readAsDataURL(file);
+});
