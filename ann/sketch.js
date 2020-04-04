3274d7629f1d7a6487296970ba5da9c49b6f73ea
vihophong <hophongmc@gmail.com>
2020-04-04 19:54:37 +0900

    aa

--- ann/sketch.js
+++ ann/sketch.js
@@ -84,28 +84,26 @@ function setup() {
 
   // normalize the data
   nn.normalizeData();
+  $('.progress-bar').hide();
+}
 
-  // train
+$("#predict-button").click(async function () {
+  $('.progress-bar').show();
+// train
   const TRAINING_OPTIONS = {
     batchSize: 2,
     epochs: 10,
   }
-  nn.train(TRAINING_OPTIONS, finishedTraining)
+  nn.train(TRAINING_OPTIONS, finishedTraining);
   $('.progress-bar').hide();
-}
+});
 
 function finishedTraining() {
   console.log("finished training");
+  testA.loadPixels();
+  const test = Array.from(testA.pixels);
+  nn.classify([test], gotResults)
 }
-
-$("#predict-button").click(async function () {
-  let irnd = getRandomInt(20)
-  let item = images[irnd];
-    // get back the image array
-  item.image.loadPixels()
-  let imageArray = Array.from(item.image.pixels);
-  nn.classify([imageArray], gotResults)
-});
 
 function gotResults(err, result) {
   if (err) {
