ad93cad92358b4f8a6a0ed4665ad8fbc693eb1f7
vihophong <hophongmc@gmail.com>
2020-04-04 13:29:39 +0900

    aa

--- _site/ann/sketch.js
+++ _site/ann/sketch.js
@@ -36,7 +36,7 @@ function preload() {
 }
 
 function setup() {
-  $('.progress-bar').show();
+  //$('.progress-bar').show();
   const options = {
     task: 'imageClassification',
     debug: true,
@@ -66,7 +66,7 @@ function setup() {
     epochs: 10,
   }
   nn.train(TRAINING_OPTIONS, finishedTraining)
-  $('.progress-bar').hide();
+  //$('.progress-bar').hide();
 }
 
 function finishedTraining() {
